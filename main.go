d24cafdd100528729681e777380edf7af3c76b37
Mohammed Salauddin <Salauddinmd1312@gmail.com>
2021-04-27 12:56:21 +0530

    Assignment

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
 	cards := newDeck()
 	hand, remainingDeck := deal(cards, 5)
@@ -8,4 +13,16 @@ func main() {
 	cards.saveToFile("myCards")
 	cards.shuffle()
 	cards.print()
+	assignment()
+}
+
+func assignment() {
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for _, value := range numbers {
+		if value%2 == 0 {
+			fmt.Println(strconv.Itoa(value) + " is even")
+		} else {
+			fmt.Println(strconv.Itoa(value) + " is odd")
+		}
+	}
 }
