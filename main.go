9790cac50203a08123d8ea0a57e7f54176264b7d
Mohammed Salauddin <Salauddinmd1312@gmail.com>
2021-04-29 14:06:56 +0530

    copy and sorting of slices

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 )
 
@@ -14,6 +15,8 @@ func main() {
 	cards.shuffle()
 	cards.print()
 	assignment()
+	sortingSlices()
+	copySliceElements()
 }
 
 func assignment() {
@@ -25,4 +28,47 @@ func assignment() {
 			fmt.Println(strconv.Itoa(value) + " is odd")
 		}
 	}
+}
+
+func sortingSlices() {
+	array := []int{3, 2, 5, 8, 6}
+	sort.Ints(array)
+	fmt.Println(array)
+
+	fruits := []string{"peach", "kiwi", "banana", "pineapple"}
+	sort.Sort(byLength(fruits))
+	fmt.Println(fruits)
+}
+
+type byLength []string
+
+func (s byLength) Len() int {
+	return len(s)
+}
+
+func (s byLength) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s byLength) Less(i, j int) bool {
+	return len(s[i]) > len(s[j])
+}
+
+func copySliceElements() {
+	slc1 := []int{58, 69, 40, 45, 11, 56, 67, 21, 65}
+	var slc2 []int
+	slc3 := make([]int, 5)
+	slc4 := []int{78, 50, 67, 77}
+
+	copy_1 := copy(slc2, slc1)
+	fmt.Println("\nSlice:", slc2)
+	fmt.Println("Total number of elements copied:", copy_1)
+
+	copy_2 := copy(slc3, slc1)
+	fmt.Println("\nSlice:", slc3)
+	fmt.Println("Total number of elements copied:", copy_2)
+
+	copy_4 := copy(slc1, slc4)
+	fmt.Println("\nSlice:", slc1)
+	fmt.Println("Total number of elements copied:", copy_4)
 }
